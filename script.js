cd4e1177b1cfd2b7a5b17c47f518a7a4043e2776
bryto <mikescottt4569@gmail.com>
2024-09-18 20:31:26 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Select all project items
+//const projectItems = document.querySelectorAll('.project-item');
+
+// Add hover effect
+//projectItems.forEach(item => {
+    //item.addEventListener('mouseenter', () => {
+        //alert(`You hovered over ${item.querySelector('h3').textContent}`);
+    //});
+//});
